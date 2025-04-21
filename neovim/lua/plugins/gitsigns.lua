838330b64129b3a66a04a18efae7cbc1e1209e2c
forkd4x <forkd4x@icloud.com>
2025-04-21 08:58:00 -0400

    Fix gitsigns deprecated functions

--- neovim/lua/plugins/gitsigns.lua
+++ neovim/lua/plugins/gitsigns.lua
@@ -10,11 +10,10 @@ return {
 		vim.keymap.set({ "o", "x" }, "ig", [[:<C-u>Gitsigns select_hunk<cr>]])
 		vim.keymap.set("n", "<leader>gl", function() gs.blame_line({ full = true }) end, { desc = "Blame Line" })
 		vim.keymap.set("n", "<leader>gp", gs.preview_hunk, { desc = "Preview Hunk" })
+		vim.keymap.set("n", "<leader>gd", gs.preview_hunk_inline, { desc = "Preview Hunk Inline" })
 		vim.keymap.set("n", "<leader>gr", gs.reset_hunk, { desc = "Reset Hunk" })
 		vim.keymap.set("x", "<leader>gr", function() gs.reset_hunk(get_range()) end, { desc = "Reset Hunk" })
-		vim.keymap.set("n", "<leader>gs", gs.stage_hunk, { desc = "Stage Hunk" })
-		vim.keymap.set("x", "<leader>gs", function() gs.stage_hunk(get_range()) end, { desc = "Stage Hunk" })
-		vim.keymap.set("n", "<leader>gu", gs.undo_stage_hunk, { desc = "Undo State Hunk" })
-		vim.keymap.set("n", "<leader>gd", gs.toggle_deleted, { desc = "Toggle Deleted" })
+		vim.keymap.set("n", "<leader>gs", gs.stage_hunk, { desc = "Stage/Unstage Hunk" })
+		vim.keymap.set("x", "<leader>gs", function() gs.stage_hunk(get_range()) end, { desc = "Stage/Unstage Hunk" })
 	end,
 }
