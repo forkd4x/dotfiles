3cfd7c09db09aff0acdb3101d5b6d10fb0b74d07
forkd4x <forkd4x@icloud.com>
2025-04-26 10:13:00 -0400

    Switch from actions-preview.nvim to tiny-code-action.nvim

---
+++ neovim/lua/plugins/tiny-code-action.lua
@@ -0,0 +1,17 @@
+return {
+	"rachartier/tiny-code-action.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"folke/snacks.nvim",
+	},
+	event = "LspAttach",
+	opts  = {
+		picker = { "snacks" },
+	},
+	keys = {
+		{ "gra", function()
+				require("tiny-code-action").code_action({})
+			end, desc = "Code Actions"
+		},
+	},
+}
