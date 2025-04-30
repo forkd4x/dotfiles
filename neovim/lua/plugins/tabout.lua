bbf748fcb4f4c8fe1cfcfb2ab9ec6fda4efbe1c6
forkd4x <forkd4x@icloud.com>
2025-04-30 10:57:52 -0400

    Add tabout.nvim plugin

---
+++ neovim/lua/plugins/tabout.lua
@@ -0,0 +1,9 @@
+return {
+	"abecodes/tabout.nvim",
+	config = function()
+		---@diagnostic disable-next-line: missing-fields
+	  require("tabout").setup({})
+		vim.keymap.set("i", "<tab>", [[<Plug>(TaboutMulti)]])
+		vim.keymap.set("i", "<S-tab>", [[<Plug>(TaboutBackMulti)]])
+	end,
+}
