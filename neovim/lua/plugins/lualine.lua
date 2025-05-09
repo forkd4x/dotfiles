bde246c85ccd89d120c09ae51b8ec44354e1cc53
forkd4x <forkd4x@icloud.com>
2025-05-09 10:19:48 -0400

    Add parrot.nvim status in lualine; Add keymap to toggle "thinking"

--- neovim/lua/plugins/lualine.lua
+++ neovim/lua/plugins/lualine.lua
@@ -1,3 +1,18 @@
+local function parrot_status()
+  if (
+    require("lazy.core.config").plugins["parrot.nvim"]._.loaded == nil
+    or vim.fn.winwidth(0) < 80
+  ) then
+    return ""
+  end
+  local info = require("parrot.config").get_status_info()
+  local is_chat = info.is_chat and "chat" or "command"
+  local provider = info.prov[is_chat].name
+  local model = string.gsub(info.model, "-20[0-9]+", "")
+  local thinking = require("parrot.config").chat_handler.providers[provider].params[is_chat].thinking
+  return string.format("{%s%s}", model, thinking and "[think]" or "")
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   opts = {
@@ -8,13 +23,18 @@ return {
     sections = {
       lualine_b = { "branch", "diff" },
       lualine_x = {
-        { "macro-recording",
+        {
+          "macro-recording",
           fmt = function()
             if vim.fn.reg_recording() == "" then return "" end
             return "Recording @" .. vim.fn.reg_recording()
           end,
         },
-        "selectioncount", "searchcount", "diagnostics", "filetype",
+        "selectioncount",
+        "searchcount",
+        { parrot_status, color = "Label" },
+        "diagnostics",
+        "filetype",
       },
     },
   },
