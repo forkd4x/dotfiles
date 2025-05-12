bda27b2cc6d4e57be8b8e204b8f49585eacf9492
forkd4x <forkd4x@icloud.com>
2025-05-12 15:42:23 -0400

    Fix cmdheight changing when using trouble.nvim
    https://github.com/kwkarlwang/bufresize.nvim/issues/13

--- neovim/lua/plugins/bufresize.lua
+++ neovim/lua/plugins/bufresize.lua
@@ -2,6 +2,15 @@ return {
   "kwkarlwang/bufresize.nvim",
   config = function()
     require("bufresize").setup()
+
+    -- Fix cmdheight changing when using trouble.nvim
+    vim.g.cmdheight = vim.o.cmdheight
+    vim.api.nvim_create_autocmd("VimResized", {
+      callback = function()
+        vim.o.cmdheight = vim.g.cmdheight
+      end
+    })
+
     -- Fix statusline disappearing on mouse click
     vim.keymap.del({ "n", "i" }, "<LeftRelease>")
   end,
