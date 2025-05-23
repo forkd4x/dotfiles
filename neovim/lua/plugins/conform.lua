dbd584c030d18b2b361e309583004fc48e3d53ac
forkd4x <forkd4x@icloud.com>
2025-05-23 14:51:00 -0400

    Add keymap to toggle conform.nvim format-on-save

--- neovim/lua/plugins/conform.lua
+++ neovim/lua/plugins/conform.lua
@@ -4,7 +4,7 @@ return {
   ft = { "go" },
   opts = {
     format_on_save = function(bufnr)
-      if vim.bo[bufnr].filetype ~= "go" then return end
+      if vim.b[bufnr].format_on_save == false then return end
       return { timeout_ms = 500, lsp_format = "fallback" }
     end,
     formatters_by_ft = {
@@ -21,6 +21,20 @@ return {
       "<leader>=", mode = { "n", "x" },
       function() require("conform").format({ lsp_format = "fallback" }) end,
       desc = "Format",
+    },
+    {
+      "<leader>+", mode = { "n", "x" },
+      function()
+        if vim.b.format_on_save == nil then
+          vim.b.format_on_save = vim.bo.filetype == "go"
+        end
+        vim.b.format_on_save = not vim.b.format_on_save
+        vim.notify(
+          string.format(
+            "%s format-on-save",
+            vim.b.format_on_save and "Enabled" or "Disabled"
+        ))
+      end, desc = "Toggle format-on-save",
     },
   },
 }
