7c3a1452ee70daed8ad1a963b187de8b56c58de3
forkd4x <forkd4x@icloud.com>
2025-06-14 10:14:00 -0400

    Display number of lines folded with custom virtual text

--- neovim/lua/plugins/nvim-ufo.lua
+++ neovim/lua/plugins/nvim-ufo.lua
@@ -12,16 +12,40 @@ return {
           winhighlight = "Normal:NormalFloat",
         },
       },
+      fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local suffix = ("  %d "):format(endLnum - lnum)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+          local chunkText = chunk[1]
+          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+          else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, {chunkText, hlGroup})
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+              suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+          end
+          curWidth = curWidth + chunkWidth
+        end
+        table.insert(newVirtText, {suffix, "Title"})
+        return newVirtText
+      end,
     })
     vim.schedule(function()
       vim.keymap.set("n", "zm", require("ufo").closeFoldsWith)
       vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
       vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
       vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-
     end)
-    vim.api.nvim_set_hl(0, "Folded", {})
-    vim.api.nvim_set_hl(0, "UfoFoldedEllipsis", { link = "FloatTitle" })
   end,
   keys = {
     {
