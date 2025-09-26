2eae4e80527aa354fd9a174236d0189cc2a94fef
forkd4x <forkd4x@icloud.com>
2025-09-26 10:15:00 -0400

    Make TreeSJ work with htmldjango files

--- neovim/lua/plugins/treesj.lua
+++ neovim/lua/plugins/treesj.lua
@@ -1,9 +1,16 @@
 return {
   "Wansmer/treesj",
   dependencies = "nvim-treesitter/nvim-treesitter",
-  opts = {
-    use_default_keymaps = false,
-  },
+  config = function()
+    local utils = require("treesj.langs.utils")
+    local html = require("treesj.langs.html")
+    require("treesj").setup({
+      use_default_keymaps = false,
+      langs = {
+        htmldjango = utils.merge_preset(html, {}),
+      },
+    })
+  end,
   keys = {
     { "<leader>j", [[:TSJSplit<cr>]], desc = "TreeSJ Split" },
     { "<leader>J", [[:TSJJoin<cr>]], desc = "TreeSJ Join" },
